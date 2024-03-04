528e3c92e746f1922fb340dbcfe731f0d7d4a60d
paubel <paul.belfrage@gmail.com>
2024-03-04 22:34:55 +0100

    Add box interaction and color update

--- script.js
+++ script.js
@@ -29,6 +29,27 @@ const createScene = function () {
   box.position.x = 0.5;
   box.position.y = 1;
 
+  const boxMaterial = new BABYLON.StandardMaterial("material", scene);
+  boxMaterial.diffuseColor = BABYLON.Color3.Random();
+  box.material = boxMaterial;
+
+  box.actionManager = new BABYLON.ActionManager(scene);
+  box.actionManager.registerAction(
+    new BABYLON.ExecuteCodeAction(
+      BABYLON.ActionManager.OnPickTrigger,
+      function (evt) {
+        const sourceBox = evt.meshUnderPointer;
+
+        //move the box upright
+        sourceBox.position.x += 0.1;
+        sourceBox.position.y += 0.1;
+
+        //update the color
+        boxMaterial.diffuseColor = BABYLON.Color3.Random();
+      }
+    )
+  );
+
   return scene;
 };
 
